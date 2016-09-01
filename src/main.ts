82f9512d567aec5f564c7407e03efb6ecf71846e
Richard Claydon <richard@ugrworks.com>
2016-09-01 15:54:53 -0400

    Merging in a stable webpack project.

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode, provide } from '@angular/core';
-import "materialize-css";
-import { AppModule }              from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule);
